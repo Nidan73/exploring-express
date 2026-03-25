77ed466dbee447da3f324cba375d0e36d655488b
Nidan73 <Nidanalam73@gmail.com>
2026-03-26 01:19:54 +0600

    added useState management to show data dynamically

--- user-managment-client/src/components/Users.jsx
+++ user-managment-client/src/components/Users.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { use } from "react";
 
 const Users = ({ usersPromise }) => {
-  const userData = use(usersPromise);
-  console.log(userData);
+  const initialUserData = use(usersPromise);
+  const [users, setUsers] = useState(initialUserData);
+  // console.log(userData);
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const newUser = { name, email };
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("My new Data", data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
+      });
+  };
   return (
     <>
       <div>
         <h2>Add user Data</h2>
-        <form>
+        <form onSubmit={handleAdd}>
           <label>Name : </label>
           <input type="text" name="name" />
           <br />
@@ -16,13 +39,12 @@ const Users = ({ usersPromise }) => {
           <input type="text" name="email" />
           <br />
           <br />
-          <br />
           <button>Add user</button>
         </form>
       </div>
 
       <div>
-        {userData.map((user) => (
+        {users.map((user) => (
           <p key={user.id}>
             {user.name} {user.email}
           </p>
