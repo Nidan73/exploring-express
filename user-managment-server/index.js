31d63c120f5f00a5f3084a3ffd6a757c2affdf76
Nidan73 <Nidanalam73@gmail.com>
2026-03-26 00:39:34 +0600

    added cors

--- user-managment-server/index.js
+++ user-managment-server/index.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("this is my first express code");
 });
 
+const users = [
+  {
+    id: 1,
+    name: "shabana",
+    email: "shabana@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Sokinna",
+    email: "Sokina@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Katrina",
+    email: "Katrina@gmail.com",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 app.listen(port, () => {
   console.log(`the server is running on this port : ${port}`);
 });
