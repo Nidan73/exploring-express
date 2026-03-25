1cbe87ac2d8b84fbed2c7b68e2def9cc4ebe813f
Nidan73 <Nidanalam73@gmail.com>
2026-03-26 01:19:30 +0600

    added like post

--- user-managment-server/index.js
+++ user-managment-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-
+app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("this is my first express code");
@@ -29,6 +29,15 @@ const users = [
 app.get("/users", (req, res) => {
   res.send(users);
 });
+
+app.post("/users", (req, res) => {
+  console.log("post is working", req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
+
 app.listen(port, () => {
   console.log(`the server is running on this port : ${port}`);
 });
